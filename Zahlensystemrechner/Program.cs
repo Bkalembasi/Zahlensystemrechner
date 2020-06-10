b3b1780a8ea611ab93374ee5dfcec122b5b9eb33
Barack <Barack@DESKTOP-MTNVSN9>
2020-06-10 09:57:57 +0200

    Projektstruktur

---
+++ Zahlensystemrechner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zahlensystemrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
