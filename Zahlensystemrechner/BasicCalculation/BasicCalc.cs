b52097b5bb495530e326d352b0985b747bfb913f
Claudio Mizia <claudio.mizia@gus-group.com>
2020-06-18 13:56:09 +0200

    Fehler korrigiert

--- Zahlensystemrechner/BasicCalculation/BasicCalc.cs
+++ Zahlensystemrechner/BasicCalculation/BasicCalc.cs
@@ -6,13 +6,15 @@ public class BasicCalc
 	public BasicCalc()
 	{
 	}
-
+    
+    //Globaler Counter zur Abfrage der Position im Result-Array
     private int counter = 0;
 
-	private double add_sub(String[] result)
+	private double Add_sub(String[] result)
     {
-        double erg = mul_div(result);
-
+        double erg = Mul_div(result);
+        
+        //Erreicht der Counter die letzte position wird abgebrochen
         if (counter == result.Length)
         {
             return erg;
@@ -22,21 +24,21 @@ public class BasicCalc
         {
             if ("+".Equals(result[counter]) || "-".Equals(result[counter]))
             {
-                erg = erg + mul_div(result);
+                erg = erg + Mul_div(result);
             } 
-
-            if (counter == result[counter])
+            if (counter == result.Length)
             {
                 return erg;
             }
         }
+        return erg;
     }
 
-	private double mul_div(String[] result)
+	private double Mul_div(String[] result)
     {
-        double erg = expo(result);
+        double erg = Expo(result);
 
-        if (counter == result[counter])
+        if (counter == result.Length)
         {
             return erg;
         }
@@ -46,29 +48,30 @@ public class BasicCalc
             if ("*".Equals(result[counter]))
             {
                 counter++;
-                erg = erg * expo(result);
+                erg = erg * Expo(result);
             }
-            else if ("/".equals(result[counter]))
+            else if ("/".Equals(result[counter]))
             {
                 counter++;
-                if (expo(result) == 0)
+                //Sollte durch 0 geteilt werden wird ein Fehler ausgegeben
+                if (Convert.ToInt32(result[counter]) == 0)
                 {
                     Console.WriteLine("Division durch 0");
-                    erg = 0;
                     return 0;
                 }
-                erg = expo(result) / erg;
+                erg = erg / Expo(result);
             }
             if (counter == result.Length)
             {
                 return erg;
             }
         }
+        return erg;
     }
 
-	private double expo(String[] result)
+	private double Expo(String[] result)
     {
-        double erg = oper(result);
+        double erg = Oper(result);
 
         if (counter == result.Length)
         {
@@ -79,7 +82,7 @@ public class BasicCalc
             if ("^".Equals(result[counter]))
             {
                 counter++;
-                erg = Math.Pow(erg, oper(result));
+                erg = Math.Pow(erg, Oper(result));
             }
             if (counter == result.Length)
             {
@@ -89,7 +92,7 @@ public class BasicCalc
         return erg;
     }
 
-    private double oper(String[] result)
+    private double Oper(String[] result)
     {
         int faktor = 1;
         double erg = 0;
@@ -104,15 +107,16 @@ public class BasicCalc
             faktor = 1;
             counter++;
         }
-        else if ("-".Equals(result[counter]))
+        if ("-".Equals(result[counter]))
         {
             faktor = -1;
             counter++;
         }
-        else if ("(".Equals(result[counter]))
+
+        if ("(".Equals(result[counter]))
         {
             counter++;
-            erg = add_sub(result);
+            erg = Add_sub(result);
             counter++;
             return erg;
         }
@@ -120,22 +124,5 @@ public class BasicCalc
         erg = System.Convert.ToDouble(result[counter]) * faktor;
         counter++;
         return erg;
-    }
-
-    public static void Main(string[] args)
-    {
-        Regex regex = new Regex("(?<=[-+*/)(^])|(?=[-+*/)(^])");
-        Regex filter = new Regex("[^0-9-+*/^)(.]");
-
-        Console.WriteLine("Rechnung eingeben: ");
-        String eingabe = Console.ReadLine();
-
-        eingabe = filter.Replace(filter, "");
-        String[] result = regex.Split(eingabe);
-
-        BasicCalc calc = new BasicCalc();
-        Double erg = calc.add_sub(result);
-
-        Console.WriteLine(erg);
     }
 }
