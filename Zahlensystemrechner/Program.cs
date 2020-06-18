5d7717abf8aa4483a97c946a960114852dcd0941
Claudio Mizia <claudio.mizia@gus-group.com>
2020-06-18 14:28:38 +0200

    Zeichnung und Thread merge

--- Zahlensystemrechner/Program.cs
+++ Zahlensystemrechner/Program.cs
@@ -9,82 +9,45 @@ using System.Threading.Tasks;
 
 namespace Zahlensystemrechner
 {
-    class Rahmen
+    class Programm
     {
         int breite = Console.WindowWidth;
         int hoehe = Console.WindowHeight;
         bool geaendert = true;
 
-        private void ZeichneRahmen()
+        private void writeUI()
         {
+            BuildRectangle rec = new BuildRectangle();
+            Calculator calc = new Calculator();
+
             while (true)
             { 
                 if (geaendert)
                 {
-
                     Console.Clear();
                     geaendert = false;
-                    Console.WriteLine(this.breite);
-                    Console.WriteLine(this.hoehe);
                     this.breite = Console.WindowWidth;
                     this.hoehe = Console.WindowHeight;
+
+                    rec.createWindowBorder();
+                    rec.writeInfoText();
+                    calc.WriteCalculator();
                 }
                 if (!(breite==Console.WindowWidth) && !(hoehe==Console.WindowHeight))
                 {
                     geaendert = true;
-
                 }
             }
         }
 
         public static void Main(string[] args)
         {
-            string term;
-            BuildRectangle rec = new BuildRectangle();
-            rec.createWindowBorder();
-            rec.writeInfoText();
+            Programm calculator = new Programm();
 
-            Calculator calc = new Calculator();
-            calc.WriteCalculator();
+            Thread ui = new Thread(new ThreadStart(calculator.writeUI));
+            ui.Start();
 
-            //Infotext();
-
-            term = Convert.ToString(Console.ReadLine());
-            
-          
-        }
-
-        public static void Infotext()
-        {
-            Console.SetCursorPosition(1,1);
-            Console.WriteLine("Zahlensystem-Rechner");
-            Console.SetCursorPosition(1,2);
-            Console.WriteLine("Infos:");
-            Console.SetCursorPosition(1, 3);
-            Console.WriteLine("Der Rechner beherrscht die Grundrechenarten + - / *");
-            Console.WriteLine("");
-            Console.SetCursorPosition(1, 5);
-            Console.WriteLine("Der Rechner beachtet Klammern und Punkt-vor-Strich");
-            Console.WriteLine("");
-            Console.SetCursorPosition(1, 7);
-            Console.WriteLine("Das Ergebnis wird mit Betätigen der Return- oder =-Taste berechnet und ausgegeben");
-            Console.WriteLine("");
-            Console.SetCursorPosition(1, 9);
-            Console.WriteLine("Zahlen der verschiedenen Zahlensysteme müssen mit dem zugehörigen Präfix gekennzeichnet werden:");
-            Console.SetCursorPosition(1, 10);
-            Console.WriteLine("Oktar: O_");
-            Console.SetCursorPosition(1, 11);
-            Console.WriteLine("Binär: B_");
-            Console.SetCursorPosition(1, 12);
-            Console.WriteLine("Hexadezimal: H_");
-            Console.SetCursorPosition(1, 13);
-            Console.WriteLine("Dezimal: ohne Präfix");
-            Console.WriteLine("");
-            Console.SetCursorPosition(1, 15);
-            Console.WriteLine("Eingabe der Rechnung:");
-            Rahmen rahmen = new Rahmen();
-            Thread t = new Thread(new ThreadStart(rahmen.ZeichneRahmen));
-            t.Start();
+            String term = Convert.ToString(Console.ReadLine());          
         }
     }
 }
